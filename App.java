c4107c0d6ce9661dc6eaa09b21409a3321b7e8d5
Cinchana1506 <163380669+Cinchana1506@users.noreply.github.com>
2025-06-02 21:56:57 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package cse.cinchana.prg10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
